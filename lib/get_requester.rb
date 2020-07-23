d5ad74246e2f06a007934b08590c0a2a4796486d
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-23 07:53:01 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,13 +5,14 @@ require 'json'
 
 class GetRequester
   
-    attr_accessor :url,:http,:response,:request
+    attr_accessor :url,:response
     
     def initialize(url)
       @url=url 
     end
   
     def get_response_body
+        
         uri=URI.parse(url)
     
         response = Net::HTTP.get_response(uri)
