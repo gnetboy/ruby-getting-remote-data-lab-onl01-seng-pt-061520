155d87db790a67a672de287422506f12aab4cc7e
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-23 07:58:32 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -5,7 +5,7 @@ require 'json'
 
 class GetRequester
   
-    attr_accessor :url,:response,:people
+    attr_accessor :url,:response,:results
     
     def initialize(url)
       @url=url 
@@ -20,9 +20,9 @@ class GetRequester
     end
 
     def parse_json
-         people=JSON.parse(self.get_response_body)
-         people.collect do |people|
-          people.to_s["people"]
+         results=JSON.parse(self.get_response_body)
+         results.collect do |result|
+         results["result"]
        end
     end
 
