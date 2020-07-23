7099d721886dec1645bcaa7f0f43972aa5337d15
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-07-23 07:51:19 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -14,7 +14,7 @@ class GetRequester
     def get_response_body
         uri=URI.parse(url)
     
-        response = Net::HTTP::Get_response(uri)
+        response = Net::HTTP.get_response(uri)
         response.body
     end
 
